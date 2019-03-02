032d5b05733c92cf07c39c4c606119e24da911a1
lvliao <git@code.aliyun.com:te_lv02/test2.git>
2019-03-02 15:06:20 +0800

    使用断路器

--- server/src/main/java/com/lv/order/OrderApplication.java
+++ server/src/main/java/com/lv/order/OrderApplication.java
@@ -1,21 +1,37 @@
 package com.lv.order;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-
-@SpringBootApplication
-@EnableEurekaClient
 @EnableFeignClients(basePackages = {"com.lv.product.client"})
-@EnableCircuitBreaker
+//@SpringBootApplication
+//@EnableEurekaClient
+//@EnableCircuitBreaker
+@SpringCloudApplication
+@EnableHystrixDashboard
+@ComponentScan(basePackages = "com.lv")
 public class OrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
     }
 
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 }
 
